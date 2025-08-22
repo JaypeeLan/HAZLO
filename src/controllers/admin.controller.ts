c907a085857bed374f6d58bf64658158080c1fed
Laniran JohnPaul <jplaniran01@gmail.com>
2025-08-22 14:54:22 +0100

    edits

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -42,6 +42,7 @@ export const createAdmin = async (
       profileImage: null,
       username: null,
       name: null,
+      role: 'admin',
       address: null,
       isVerified: true,
       verificationToken,
