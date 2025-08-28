9d797c040a57532a3a00afd3e9c6ed9882630edf
Laniran JohnPaul <jplaniran01@gmail.com>
2025-08-28 04:38:04 +0100

    admin analytics, updated price list

--- src/controllers/services.controller.ts
+++ src/controllers/services.controller.ts
@@ -7,30 +7,39 @@ export const getPriceList = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   try {
-    const { service } = req.query;
-
-    // if (
-    //   service &&
-    //   !['gas', 'laundry'].includes(service.toString().toLowerCase())
-    // ) {
-    //   throw new AppError('Invalid service type', 400);
-    // }
+    const { type } = req.query;
 
     const filter: Record<string, any> = {};
-    if (service) {
-      filter.service = service.toString().toLowerCase();
+    if (type) {
+      const validTypes = ['gas_order', 'gas_refill', 'laundry'];
+      const typeString = type.toString().toLowerCase();
+      if (!validTypes.includes(typeString)) {
+        throw new AppError('Invalid type provided', 400);
+      }
+      filter['prices.type'] = typeString;
     }
 
-    const services = await PriceListModel.find(filter);
+    const priceList = await PriceListModel.findOne(filter);
+    if (!priceList) {
+      throw new AppError('Price list not found', 404);
+    }
+
+    const responseData = type
+      ? {
+          prices: priceList.prices.filter(
+            (p) => p.type === type.toString().toLowerCase()
+          ),
+        }
+      : priceList;
 
     sendResponse({
       res,
       statusCode: 200,
       status: 'success',
-      message: 'Services fetched',
-      data: services,
+      message: 'Price list fetched successfully',
+      data: responseData,
     });
   } catch (error) {
     next(
