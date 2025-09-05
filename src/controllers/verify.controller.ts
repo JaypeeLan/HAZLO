cece6a77364ba98b9ebafcce7547ffc6a4b2d1ee
Laniran JohnPaul <jplaniran01@gmail.com>
2025-09-05 12:54:32 +0100

    build err

--- src/controllers/verify.controller.ts
+++ src/controllers/verify.controller.ts
@@ -4,7 +4,7 @@ import UserModel from '../models/user';
 import { ResponseMessages } from '../utils/constants';
 
 import { sendResponse } from '../utils/sendResponse';
-import { twilioClient, twilioServiceSid } from '../utils/twilio';
+// import { twilioClient, twilioServiceSid } from '../utils/twilio';
 import {
   formatUser,
   generateVerificationToken,
@@ -12,80 +12,80 @@ import {
 } from '../utils/helpers';
 import { sendEmail } from '../services/mail';
 
-export const sendOtp = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const { phone } = req.body;
+// export const sendOtp = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ): Promise<void> => {
+//   try {
+//     const { phone } = req.body;
 
-    if (!phone) {
-      throw new AppError(ResponseMessages.INVALID_PHONE, 400);
-    }
+//     if (!phone) {
+//       throw new AppError(ResponseMessages.INVALID_PHONE, 400);
+//     }
 
-    const verification = await twilioClient.verify.v2
-      .services(twilioServiceSid)
-      .verifications.create({ to: phone, channel: 'sms' });
+//     const verification = await twilioClient.verify.v2
+//       .services(twilioServiceSid)
+//       .verifications.create({ to: phone, channel: 'sms' });
 
-    sendResponse({
-      res,
-      statusCode: 200,
-      status: 'success',
-      message: ResponseMessages.OTP_SENT,
-      data: {
-        phone: verification.to,
-        numberOfAttempts: verification.sendCodeAttempts.length,
-      },
-    });
-  } catch (error) {
-    console.error('Send OTP error:', error);
-    next(new AppError(ResponseMessages.OTP_SEND_ERROR, 500));
-  }
-};
+//     sendResponse({
+//       res,
+//       statusCode: 200,
+//       status: 'success',
+//       message: ResponseMessages.OTP_SENT,
+//       data: {
+//         phone: verification.to,
+//         numberOfAttempts: verification.sendCodeAttempts.length,
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Send OTP error:', error);
+//     next(new AppError(ResponseMessages.OTP_SEND_ERROR, 500));
+//   }
+// };
 
-export const verifyOtp = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const { phone, otp } = req.body;
+// export const verifyOtp = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ): Promise<void> => {
+//   try {
+//     const { phone, otp } = req.body;
 
-    if (!phone || !otp) {
-      throw new AppError(ResponseMessages.INVALID_OTP, 400);
-    }
+//     if (!phone || !otp) {
+//       throw new AppError(ResponseMessages.INVALID_OTP, 400);
+//     }
 
-    const verificationCheck = await twilioClient.verify.v2
-      .services(twilioServiceSid)
-      .verificationChecks.create({ to: phone, code: otp });
+//     const verificationCheck = await twilioClient.verify.v2
+//       .services(twilioServiceSid)
+//       .verificationChecks.create({ to: phone, code: otp });
 
-    if (verificationCheck.status !== 'approved') {
-      throw new AppError(ResponseMessages.OTP_INVALID, 400);
-    }
+//     if (verificationCheck.status !== 'approved') {
+//       throw new AppError(ResponseMessages.OTP_INVALID, 400);
+//     }
 
-    const user = await UserModel.findOne({ phone });
+//     const user = await UserModel.findOne({ phone });
 
-    if (user) {
-      await updateUserVerification(user);
-    }
+//     if (user) {
+//       await updateUserVerification(user);
+//     }
 
-    sendResponse({
-      res,
-      statusCode: 200,
-      status: 'success',
-      message: ResponseMessages.OTP_VERIFY_SUCCESS,
-      data: null,
-    });
-  } catch (error) {
-    console.error('Verify OTP error:', error);
-    next(
-      error instanceof AppError
-        ? error
-        : new AppError(ResponseMessages.OTP_ERROR, 500)
-    );
-  }
-};
+//     sendResponse({
+//       res,
+//       statusCode: 200,
+//       status: 'success',
+//       message: ResponseMessages.OTP_VERIFY_SUCCESS,
+//       data: null,
+//     });
+//   } catch (error) {
+//     console.error('Verify OTP error:', error);
+//     next(
+//       error instanceof AppError
+//         ? error
+//         : new AppError(ResponseMessages.OTP_ERROR, 500)
+//     );
+//   }
+// };
 
 export const sendEmailVerification = async (
   req: Request,
